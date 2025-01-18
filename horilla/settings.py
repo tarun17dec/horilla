75d2250c1d67684d7ef92816ffa3efa88f0210e0
tarun17dec <tarun.mirchandani@falconincentives.com>
2025-01-18 12:24:39 +0530

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -126,7 +126,7 @@ else:
                 "DB_NAME",
                 default=os.path.join(
                     BASE_DIR,
-                    "TestDB_Horilla.sqlite3",
+                    "FalconDB_Horilla.sqlite3",
                 ),
             ),
             "USER": env("DB_USER", default=""),
